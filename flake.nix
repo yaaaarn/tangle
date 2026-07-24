4998586a16a94196beb9af3ac2b3cabd025b6eea
yaaaarn <30006414+yaaaarn@users.noreply.github.com>
2026-07-24 19:08:55 +0200

    this stuff is not supported on darwin

--- flake.nix
+++ flake.nix
@@ -11,8 +11,6 @@
       supportedSystems = [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
       ];
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
